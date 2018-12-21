6b92a3cea2303195b35109365ac619af2580e4e6
Oscar Gonzalo Silva Anchante <ogsilvaa@gmail.com>
2018-12-20 21:59:55 -0500

    extension AgregarSeleccione

---
+++ Pagos/App_Code/Extensiones.cs
@@ -0,0 +1,46 @@
+﻿using Pagos.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Pagos
+{
+    public static class Extensiones
+    {
+        static Guid guid;
+        public static void AgregarSeleccione(this List<ParametrosDetalle> lista)
+        {
+            lista.Insert(
+                0,
+                new ParametrosDetalle
+                {
+                    ParametroDetalleId = guid,
+                    ParametroDetalleDescripcion = "--Seleccione--"
+                });
+        }
+        public static void AgregarSeleccione<T>(this List<T> lista, string id, string value) where T : new()
+        {
+            var newT = new T();
+            var tipo = typeof(T);
+            var propiedades = tipo.GetProperties();
+            foreach (var propiedad in propiedades)
+            {
+                if(propiedad.Name==id)
+                {
+                    propiedad.SetValue(newT, guid);
+                    break;
+                }
+            }
+            foreach (var propiedad in propiedades)
+            {
+                if (propiedad.Name == value)
+                {
+                    propiedad.SetValue(newT, "--Seleccione--");
+                    break;
+                }
+            }
+            lista.Insert(
+                0,
+                newT);
+        }
+    }
+}
